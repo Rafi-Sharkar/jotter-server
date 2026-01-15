8f93cf28e40995b03ae41f6a18cb695f46c25d9c
Rafi-Sharkar <rafisharkar144@gmail.com>
2026-01-16 05:05:17 +0600

    Jotter_v2

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-run docker container: jotter-server
+To run this project and get APIs in swagger follow this steps
 ```
-docker compose --profile dev up -d --build
+0. git clone https://github.com/Rafi-Sharkar/jotter-server.git
+1. pnpm i [if pnpm is installed]
+2. docker compose --profile dev up -d --build
+3. rename [.env.example => .env]
+4. pnpm dev
+5. http://localhost:5000/docs   [To show Swagger APIs]
 
 ```
+
+In this project i user my SMPT and Cloudinary is limited
--- src/lib/file/file.module.ts
+++ src/lib/file/file.module.ts
@@ -2,11 +2,10 @@ import { Global, Module } from '@nestjs/common';
 import { CloudinaryService } from './services/cloudinary.service';
 import { FileService } from './services/file.service';
 import { MulterService } from './services/multer.service';
-import { S3Service } from './services/s3.service';
 
 @Global()
 @Module({
-  providers: [FileService, S3Service, MulterService, CloudinaryService],
-  exports: [FileService, S3Service, MulterService, CloudinaryService],
+  providers: [FileService, MulterService, CloudinaryService],
+  exports: [FileService, MulterService, CloudinaryService],
 })
 export class FileModule {}
